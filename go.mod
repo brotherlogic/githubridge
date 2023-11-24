498378dd1a600b4c6596e23b5f353b08d85ef90a
Simon Tucker <brotherlogic@gmail.com>
2023-11-24 13:54:08 -0800

    First pass

---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/brotherlogic/githubridge
+
+go 1.20
+
+require (
+	github.com/google/go-github/v56 v56.0.0
+	github.com/prometheus/client_golang v1.17.0
+	golang.org/x/oauth2 v0.14.0
+	google.golang.org/grpc v1.59.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
+)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/google/go-github/v56/github"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/oauth2"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+
+	pb "github.com/brotherlogic/githubridge/proto"
+)
+
+var (
+	port        = flag.Int("port", 8080, "Server port for grpc traffic")
+	metricsPort = flag.Int("metrics_port", 8081, "Metrics port")
+	owner       = flag.String("owner", "brotherlogic", "")
+)
+
+type Server struct {
+	client *github.Client
+	owner  string
+}
+
+func (s *Server) AddIssue(ctx context.Context, req *pb.AddIssueRequest) (*pb.AddIssueResponse, error) {
+	issue, resp, err := s.client.Issues.Create(ctx, s.owner, req.GetJob(), &github.IssueRequest{
+		Title: proto.String(req.GetTitle()),
+		Body:  proto.String(req.GetBody()),
+	})
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 201 {
+		return nil, fmt.Errorf("Bad response code: %v", resp.StatusCode)
+	}
+
+	return &pb.AddIssueResponse{IssueId: int32(issue.GetID())}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	token := os.Getenv("GITHUBRIDGE_TOKEN")
+	if token == "" {
+		log.Fatalf("You must specify the token")
+	}
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(context.Background(), ts)
+
+	s := &Server{
+		owner:  *owner,
+		client: github.NewClient(tc),
+	}
+
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		err := http.ListenAndServe(fmt.Sprintf(":%v", *metricsPort), nil)
+		log.Fatalf("gramophile is unable to serve metrics: %v", err)
+	}()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("gramophile is unable to listen on the grpc port %v: %v", *port, err)
+	}
+	gs := grpc.NewServer()
+	pb.RegisterGithubBridegServer(gs, s)
+	if err := gs.Serve(lis); err != nil {
+		log.Fatalf("gramophile is unable to serve grpc: %v", err)
+	}
+	log.Fatalf("gramophile has closed the grpc port for some reason")
+}
