d07623b4c63a83e11f1a933150ebb592572cf200
Simon Tucker <brotherlogic@gmail.com>
2024-02-19 23:53:31 -0800

    Better logging

--- server/webhook.go
+++ server/webhook.go
@@ -17,7 +17,7 @@ func (s *Server) githubwebhook(w http.ResponseWriter, r *http.Request) {
 
 	event, err := github.ParseWebHook(github.WebHookType(r), body)
 	if err != nil {
-		log.Printf("Bad stuff")
+		log.Printf("Bad stuff: %v", err)
 	}
 	switch event := event.(type) {
 	case *github.IssueEvent:
@@ -25,6 +25,6 @@ func (s *Server) githubwebhook(w http.ResponseWriter, r *http.Request) {
 		action := event.Event
 		log.Printf("%v -> %v", repo, action)
 	default:
-		log.Printf("Unable to process %v", event)
+		log.Printf("Unable to process %v (%T)", event, event)
 	}
 }
