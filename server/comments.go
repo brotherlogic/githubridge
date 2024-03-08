a0bb0aef1ba8cde34f21a66c72fd1d79c8fa6a8f
Simon Tucker <brotherlogic@gmail.com>
2024-03-07 19:58:03 -0800

    Fixed minor bugs

--- server/comments.go
+++ server/comments.go
@@ -7,8 +7,6 @@ import (
 	"google.golang.org/grpc/status"
 
 	"github.com/google/go-github/v50/github"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 
 	pb "github.com/brotherlogic/githubridge/proto"
 )
@@ -21,7 +19,7 @@ func convertComment(comment *github.IssueComment) *pb.Comment {
 }
 
 func (s *Server) GetComments(ctx context.Context, req *pb.GetCommentsRequest) (*pb.GetCommentsResponse, error) {
-	results, ghr, err := s.client.Issues.ListComments(ctx, req.GetUser(), req.GetRepo(), int(req.GetNumber()), &gituhb.IssueListCommentsOptions{})
+	results, ghr, err := s.client.Issues.ListComments(ctx, req.GetUser(), req.GetRepo(), int(req.GetId()), &github.IssueListCommentsOptions{})
 
 	if err != nil {
 		return nil, err
