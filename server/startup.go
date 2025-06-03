503b24114b66aaf72c24e9bca4ca8d287ed57542
Simon Tucker <brotherlogic@gmail.com>
2025-06-02 17:10:45 -0700

    Handle bad read

--- server/startup.go
+++ server/startup.go
@@ -129,6 +129,9 @@ func (s *Server) startup(ctx context.Context) error {
 			ListOptions: github.ListOptions{Page: cpage},
 			Type:        "all",
 		})
+		if err != nil {
+			return err
+		}
 		lpage = resp.LastPage
 		log.Printf("READ: %v / %v (%v)", cpage, resp.LastPage, len(s.repos))
 		if err != nil {
